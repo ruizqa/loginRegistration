9395174fd1daaf477860bc471234612c7eb9a410
ruizqa <89569188+ruizqa@users.noreply.github.com>
2021-09-23 13:26:15 -0600

    first commit

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,68 @@
+from flask_app import app
+from flask import render_template,redirect,request,session,flash
+from flask_app.models.user import User
+from flask_bcrypt import Bcrypt        
+bcrypt = Bcrypt(app)
+#import datetime to change date format
+
+
+@app.route("/")
+def form():
+    return render_template("create.html")
+
+
+@app.route('/register', methods=["POST"])
+def create_user():
+    # First we make a data dictionary from our request.form coming from our template.
+    # The keys in data need to line up exactly with the variables in our query string.
+    if not User.validate_user(request.form):
+        # we redirect to the template with the form.
+            return redirect('/')
+    print(request.form['pw'])
+    pw_hash = bcrypt.generate_password_hash(request.form['pw'])
+    
+    data = {
+        "first_name": request.form["fname"],
+        "last_name" : request.form["lname"],
+        "email" : request.form["email"],
+        "password" : pw_hash
+    }
+    
+    # We pass the data dictionary into the save method from the User class.
+    User.save(data)
+    # Don't forget to redirect after saving to the database.
+    session['email'] = data['email']
+    return redirect('/homepage')            
+
+@app.route('/login', methods=["POST"])
+def login_user():
+    # First we make a data dictionary from our request.form coming from our template.
+    # The keys in data need to line up exactly with the variables in our query string.
+
+    data = {
+        "email" : request.form["email"],
+        "password" : request.form["pw"]
+    }
+
+    user= User.login(data)
+    print("data is ",data)
+    print("user is", user)
+    if not user:
+        return redirect('/')
+    elif not bcrypt.check_password_hash(user['password'], data['password']):
+        flash("The password is incorrect", "login")
+        return redirect('/')
+    session['email'] = data['email']
+    # Don't forget to redirect after saving to the database.
+    return redirect('/homepage')  
+
+
+@app.route("/homepage")
+def read():
+    # call the get all classmethod to get all users
+    data={
+        'email': session['email']
+    }
+    user= User.get_user_info(data)
+    return render_template("read.html", user=user)
+
---
+++ flask_app/models/user.py
@@ -0,0 +1,58 @@
+# import the function that will return an instance of a connection
+import re
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+# model the class after the friend table from our database
+class User:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+# Now we use class methods to query our database
+    @staticmethod
+    def validate_user( user ):
+        is_valid = True
+        # test whether a field matches the pattern
+        if not EMAIL_REGEX.match(user['email']): 
+            flash("Invalid email address!", "register")
+            is_valid = False
+        if len(user['fname']) < 1:
+            flash("First Name must be at least 1 character.", "register")
+            is_valid = False
+        if len(user['lname']) < 1:
+            flash("Last Name must be at least 1 character.", "register")
+            is_valid = False
+        if len(user['pw']) < 3:
+            flash("Password must be at least 3 characters.", "register")
+            is_valid = False
+        if user['pw'] != user['cpw']:
+            flash("Your password does not match the password confirmed", "register")
+            is_valid = False
+        return is_valid
+    @classmethod
+    def login(cls,data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        user = connectToMySQL('user_login').query_db(query,data)
+        if len(user) <1:
+            flash("The email provided does not belong to any of our users. Please register", "login")
+            return False
+        print(user)
+        return user[0]
+    @classmethod
+    def get_user_info(cls,data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        user = connectToMySQL('user_login').query_db(query,data)
+        return user[0]
+    @classmethod
+    def save(cls, data ):
+        query = "INSERT INTO users ( first_name , last_name , email , password, created_at, updated_at ) VALUES ( %(first_name)s , %(last_name)s , %(email)s ,%(password)s, NOW() , NOW() );"
+        # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('user_login').query_db( query, data )
